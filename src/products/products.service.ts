072b63aea50750807f97766806c9d554b311db88
MounirZS97 <mounir.zs97@gmail.com>
2021-05-20 17:31:23 +0200

    base project created

---
+++ src/products/products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './interfaces/Product';
+import { CreateProductDTO } from './dto/create-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(@InjectModel('Product') private ProductModel: Model<Product>) {}
+
+  async getProducts() {
+    return await this.ProductModel.find();
+  }
+
+  async getProduct(id: string) {
+    return await this.ProductModel.findById(id);
+  }
+
+  async createProduct(Product: CreateProductDTO) {
+    const newProduct = new this.ProductModel(Product);
+    return await newProduct.save();
+  }
+}
